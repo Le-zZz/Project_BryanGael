324ac0ee79f94fb8386276ee9c30bc9eac545c95
Bryan <bryancrim@gmail.com>
2020-02-07 15:22:10 +0100

    EnemyDmg & PlayerDmg scripts added

---
+++ Assets/Scripts/PlayerDmg.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDmg : MonoBehaviour
+{
+    private int health = 3;
+
+    void Update()
+    {
+        if (health == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "playerDmg")
+        {
+            health -= 1;
+            Debug.Log(health);
+        }
+    }
+    
+}

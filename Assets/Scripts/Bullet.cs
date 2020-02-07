7bf43b23e583f2a96e2fab667fa4a10f44d7e576
Bryan <bryancrim@gmail.com>
2020-02-07 13:19:43 +0100

    Fix Scripts

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,20 +5,13 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-<<<<<<< HEAD
     private void Start()
     {
-        Destroy(gameObject,1f);
+        Destroy(gameObject, 1f);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject);
-=======
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        Destroy(gameObject,1f);
->>>>>>> master
     }
-
 }
